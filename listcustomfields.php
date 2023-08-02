7f85346a3267e061734117c35e6400021f73e2a3
Brian Shaughnessy <brian@lcdservices.biz>
2023-08-02 08:51:25 -0400

    civix upgrade

--- listcustomfields.php
+++ listcustomfields.php
@@ -22,59 +22,12 @@ function listcustomfields_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function listcustomfields_civicrm_postInstall() {
-  _listcustomfields_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function listcustomfields_civicrm_uninstall() {
-  _listcustomfields_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function listcustomfields_civicrm_enable() {
   _listcustomfields_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function listcustomfields_civicrm_disable() {
-  _listcustomfields_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function listcustomfields_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _listcustomfields_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function listcustomfields_civicrm_entityTypes(&$entityTypes) {
-  _listcustomfields_civix_civicrm_entityTypes($entityTypes);
 }
 
 /**
